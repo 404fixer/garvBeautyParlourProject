9299e08e01ed0cff534d76423f89ae7f61856e98
Govind Tomar <govindtomar94@gmail.com>
2020-07-09 23:00:13 +0530

    service section added

--- BackEndPart/public/javascript/index.js
+++ BackEndPart/public/javascript/index.js
@@ -18,3 +18,19 @@ $(function() {
         scroll = $(document).scrollTop()
     })
 })
+
+var serviceBtn = document.querySelectorAll('#serviceBtn')
+var serviceCardHidden = document.querySelectorAll('#serviceCardHidden')
+
+console.log(serviceBtn[4])
+console.log(serviceCardHidden[4])
+
+for (let i = 0; i < serviceBtn.length; i++) {
+    serviceBtn[i].addEventListener('click', function() {
+        serviceCardHidden[i].classList.add('open')
+    })
+
+    serviceCardHidden[i].addEventListener('click', function() {
+        serviceCardHidden[i].classList.remove('open')
+    })
+}
